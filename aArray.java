ee4fb9d968304b7a8f883ab44e57f4929ba36cc3
Ceddy Muhoza <radrwinc@gmail.com>
2016-11-15 21:30:58 +0200

    Create aArray.java

---
+++ aArray.java
@@ -0,0 +1,69 @@
+package ceddy.ajds;
+
+import android.content.Context;
+
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+/**
+ * Created by Vuga Inc.
+ */
+public class allArray {
+
+    private final String USER_FILE;
+    Context context;
+
+    public allArray(Context context, String database){
+        this.context = context;
+        USER_FILE = database;
+    }
+
+    public void writeUserFile(JSONArray me){
+        if(checkUserFile())
+            deleteUserFile();
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = context.openFileOutput(USER_FILE, Context.MODE_PRIVATE);
+            outputStream.write(me.toString().getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public JSONArray readUserFile(){
+        File dir = context.getFilesDir();
+        File file = new File(dir, USER_FILE);
+        StringBuilder text = new StringBuilder();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            return new JSONArray(text.toString());
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void deleteUserFile(){
+        File dir = context.getFilesDir();
+        File file = new File(dir, USER_FILE);
+        file.delete();
+    }
+
+    public boolean checkUserFile(){
+        File dir = context.getFilesDir();
+        File file = new File(dir, USER_FILE);
+        return file.exists();
+    }
+}
+
